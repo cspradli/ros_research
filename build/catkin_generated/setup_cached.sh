590284a4b7a08e3176468fea10cc104fc814e268
Caleb Spradlin <cspradli@unca.edu>
2020-04-08 10:27:11 -0400

    fixed hashbang error

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/cspradli/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cspradli/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/cspradli/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/cspradli/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/cspradli/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/cspradli/catkin_ws/src:$ROS_PACKAGE_PATH"
