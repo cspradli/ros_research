27a5156378b78bd9e2d9abd548192c3fe9f4cca9
Caleb Spradlin <cspradli@unca.edu>
2020-04-08 15:35:31 -0400

    added new arduino link dir

---
+++ build/arduino_link/cmake_install.cmake
@@ -0,0 +1,54 @@
+# Install script for directory: /home/cspradli/catkin_ws/src/arduino_link
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/cspradli/catkin_ws/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/cspradli/catkin_ws/build/arduino_link/catkin_generated/installspace/arduino_link.pc")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/arduino_link/cmake" TYPE FILE FILES
+    "/home/cspradli/catkin_ws/build/arduino_link/catkin_generated/installspace/arduino_linkConfig.cmake"
+    "/home/cspradli/catkin_ws/build/arduino_link/catkin_generated/installspace/arduino_linkConfig-version.cmake"
+    )
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/arduino_link" TYPE FILE FILES "/home/cspradli/catkin_ws/src/arduino_link/package.xml")
+endif()
+
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/cspradli/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/cspradli/catkin_ws/build/arduino_link/cmake_install.cmake")
   include("/home/cspradli/catkin_ws/build/hello_world/cmake_install.cmake")
 
 endif()
